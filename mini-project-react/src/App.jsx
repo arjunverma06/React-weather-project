1d95273fe8f4d63690d395518b9ddbc627d2b3b5
arjunverma06 <arjunverma5319@gmail.com>
2024-07-11 15:42:00 +0530

    Ol

--- mini-project-react/src/App.jsx
+++ mini-project-react/src/App.jsx
@@ -1,11 +1,11 @@
 
 // import './App.css'
 // import Button from '@mui/material/Button';
-import Searchbox from "./Searchbox"
+// import Searchbox from "./Searchbox"
 import WeatherApp from "./WeatherApp"
 // import TextField from '@mui/material/TextField';
 // import Button from '@mui/material/Button';
-import InfoBox from "./InfoBox"
+// import InfoBox from "./InfoBox"
 
 function App() {
  
